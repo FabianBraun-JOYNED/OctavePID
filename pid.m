8b1198b2743296f3946ea2c88db2d7fb2cddd42d
Fabian Braun <fabian.braun@dbaudio.com>
2017-09-15 13:17:54 +0200

    Plot absolute error. Delay of error caculation configurable now.

--- pid.m
+++ pid.m
@@ -4,7 +4,10 @@
 ###########################################
 
 clear;
-length = 500
+# Number of samples taken for the simulation
+length = 300;
+# calculate error based on samples in the past
+offset = 2;
 
 # t_ptp is the common time base of two systems
 t_ptp = 0:1:length; # startpoint : increment value for common time base : endpoint
@@ -20,25 +23,29 @@ t_media_step = 1.1; # 1.1 run faster than time reference
 t_media_ist = t_ptp;
 
 # t_media_error is added as error signal to t_media_ist
-t_media_error = rand(length+1); 
+t_media_error = rand(length+1)*5-2.5; 
 #t_media_error = sin(t_ptp*0.5);
 
 # Control loop  
-for i=2:1:length
+for i=2+offset:1:length
   t_media_ist(i) = t_media_ist(i) + t_media_step + t_media_error(i);
-  ierror(i) = t_media_ist(i) - t_media_soll(i);
-  perror(i) = ierror(i) - ierror(i-1);
-  t_media_ist(i+1) = t_media_ist(i) - ierror(i) * 0.1 - perror(i) * 0.9;
+  ierror(i) = t_media_ist(i-offset) - t_media_soll(i-offset);
+  perror(i) = ierror(i-offset) - ierror(i-offset-1);
+  t_media_ist(i+1) = t_media_ist(i) - ierror(i) * 0.05 - perror(i) * 0.2;
 endfor
 ierror(length+1) = 0;
 perror(length+1) = 0;
    
+# Claculate absolute error
+error = t_media_ist - t_media_soll;
+
+     
 # Plot the timing values 
 figure(1);
 plot (t_ptp, t_ptp, '-', t_ptp, t_media_soll, '--', t_ptp, t_media_ist, '--');
-axis ([0 length+1 -10 length+10]); #[x_lo x_hi y_lo y_hi]
+#axis ([0 length+1 -10 length+10]); #[x_lo x_hi y_lo y_hi]
 
 # Plot the error signals
 figure(2);
-plot ( t_ptp, t_media_error, '-', t_ptp, ierror, '--', t_ptp, perror, '.');
-axis ([0 length+1 -20 20]); #[x_lo x_hi y_lo y_hi]
+plot ( t_ptp, error, '-', t_ptp, ierror, '--', t_ptp, perror, '.');
+#axis ([0 length+1 -20 20]); #[x_lo x_hi y_lo y_hi]
